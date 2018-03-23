8abee4e18bb7db0b0ea557183997c3241df3ec7c
LLzen <LLzen@users.noreply.github.com>
2018-03-23 16:16:05 +0100

    dev

--- NettOp-plugin.php
+++ NettOp-plugin.php
@@ -3,6 +3,23 @@
 Plugin Name: NettOp-plugin
 Plugin URI: http://www.uis.no
 Description: NettOp demo plugin
+
+Adds 'NettOp settings' option to Dashboard->Settings
+Options (see settings page for more info):
+Google Fonts - load google font
+CSS - site level css
+
+Shortcodes:
+NETTOP_getLinkIcons - see custom field 'NETTOP_getLinkIconTemplate'
+NETTOP_getPreviousLink 
+NETTOP_getNextLink
+NETTOP_getConfigValue
+
+Via Custom fields:
+NETTOP_getLinkIconTemplate - combine with shortcode 'NETTOP_getLinkIcons'. This provides the template and should contain formatting around [link], [image] and [title]
+NETTOP_pageCss - provides page level css
+
+
 */
 
 require( dirname( __FILE__ ) . '/settings.php' ); //include the settings menu option
@@ -37,21 +54,35 @@ class nettop {
 
     private static $pages=array();
 
-    private static $my_settings_page;
+    private static $nettop_settings_page;
 
     public function setup() {
 
-        self::$my_settings_page = new MySettingsPage();
+        self::$nettop_settings_page = new NettopSettingsPage();
 
         add_shortcode(NETTOP_SHORTCODE . "getPreviousLink", array( $this, 'getPreviousLink'));
         add_shortcode(NETTOP_SHORTCODE . "getNextLink", array( $this, 'getNextLink'));
         add_shortcode(NETTOP_SHORTCODE . "getConfigValue", array( $this, 'getConfigValue'));
+        add_shortcode(NETTOP_SHORTCODE . "getLinkIcons", array( $this, 'getLinkIcons'));
 
+        //wordpress attempts to improve texts visually - which breaks the data for certain shortcodes, so we have to add some exemptions
+        add_filter( 'no_texturize_shortcodes', array( $this, 'shortcodes_to_exempt_from_wptexturize'));
         
+        //add_action('wp_head', array( $this, 'addJavascript'));
         if (is_null(self::getConfig(NETTOP_DATA_googleFont))==false) {
             add_action('wp_enqueue_scripts', array( $this, 'addGoogleFonts_loader'));
             add_action('wp_head', array( $this, 'addGoogleFonts_css'));
         }
+
+        if (is_null(self::getConfig("css"))==false) {
+            add_action('wp_head', array( $this, 'addplugincss'));
+        }
+
+        if (is_null(self::getConfig("css"))==false) {
+            add_action('wp_head', array( $this, 'addpagecss'));
+        }
+        
+
 
         //create a simple list of all pages - in the menu sequence
         $args = array(
@@ -78,9 +109,17 @@ class nettop {
         }
     }
 
+
+    //we want to stop wordpress from altering the data and text!
+    function shortcodes_to_exempt_from_wptexturize( $shortcodes ) {
+        $shortcodes[] = 'NETTOP_getLinkIcons';
+        return $shortcodes;
+    }
+    
+
     //shortcode
     function getConfigValue($attsIn = [], $content = null, $tag) {
-        //var_dump(self::$my_settings_page); //use to view the lot
+        //var_dump(self::$nettop_settings_page); //use to view the lot
 
         // normalize attribute keys, lowercase
         $attsIn = array_change_key_case((array)$attsIn, CASE_LOWER);
@@ -96,7 +135,7 @@ class nettop {
         if (empty($atts["id"])) {
             $ret="NettOp plugin error: getConfigValue called but no id set!";
         } else {
-            //$ret=self::$my_settings_page->getOption($atts["id"]);
+            //$ret=self::$nettop_settings_page->getOption($atts["id"]);
             $ret=self::getConfig($atts["id"]);
             if ($ret==null) {
                 $ret="NettOp plugin error: config value '" . $atts["id"] . "' requested - but nothing by that name exists.";
@@ -112,7 +151,7 @@ class nettop {
             case "dummy":
                 return "dummy";
             default:
-                $ret=self::$my_settings_page->getOption($id);;
+                $ret=self::$nettop_settings_page->getOption($id);;
                 if (empty($ret)) {$ret=null;}
                 return $ret;
         }
@@ -131,15 +170,31 @@ class nettop {
       <?php
     }
 
-        
-/*
+
+    function addplugincss(){
+        $css=self::getConfig("css");
+      ?>
+      <style type="text/css">
+        <?php echo($css); ?>
+      </style>
+      <?php
+    }   
+
+    function addpagecss(){
+        global $post;
+        $css=get_post_meta($post->ID, NETTOP_SHORTCODE . 'pageCss', true);
+      ?>
+      <style type="text/css">
+        <?php echo($css); ?>
+      </style>
+      <?php
+    }         
+
     function addJavascript(){ //in header
       ?>
       <script>alert( 'Hi Marmite' ); </script>
       <?php
     }
-*/
-
 
     function getNextLink( $atts, $content ) {
         $current = array_search(get_the_ID(), self::$pages);
@@ -158,6 +213,100 @@ class nettop {
             return get_permalink(self::$pages[$current-1]);
         }
     }
+
+
+
+    function getLinkIcons( $atts, $content ) {
+        global $post;
+
+        //defaults (lowercase!)
+        $atts = shortcode_atts( array(
+            'iconwidth' => '100%',
+            'iconheight' => '100%',
+        ), $atts );        
+
+        $icons=self::get_shortcode_from_content($content, "icon", false);
+        //var_dump($icons);
+
+        //var_dump($atts["iconwidth"]);
+
+        $template=get_post_meta($post->ID, NETTOP_SHORTCODE . 'getLinkIconTemplate', true);
+
+        if ($template=="") {
+            $ret="Error: shortcode '" . NETTOP_SHORTCODE . "getLinkIcons' being used - but not template has been defined!";
+        } else {
+
+            
+            //var_dump($template);
+
+            $ret="<div class=\"items_outer\" style=\"display: block;\">";
+            foreach ($icons as $icon) {
+                $build=$template;
+                $text=$icon->atts["text"];
+                $link=$icon->atts["link"];
+                $image=$icon->atts["image"];
+                
+                if ($image==="") {$image='data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==';}
+                
+                $build=str_replace("[text]",$text,$template);
+                $build=str_replace("[link]",$link,$build);
+                $build=str_replace("[image]",$image,$build);
+                $build=str_replace("<img ","<img width=\"". $atts["iconwidth"] . "\" height=\"" . $atts["iconheight"] . "\" ",$build);
+                $ret=$ret . $build;
+                //var_dump($build);
+                //echo("<br>");
+            }
+            $ret=$ret . "</div>";
+        }
+        return $ret;
+    }
+
+    //find specified shortcodes in content
+    //return array of objects - finds shortcodes called $tag
+    //td: parsePosh should now NOT be needed since adding shortcodes_to_exempt_from_wptexturize should fix the proper way!
+    function get_shortcode_from_content( $content, $tag) {
+        //$content=$post->post_content;
+        $ret = array();
+        $regex=get_shortcode_regex(array($tag)); //we only want to filter the supplied one
+        //var_dump($regex);
+        if ( preg_match_all( '/' . $regex . '/s', $content, $matches, PREG_SET_ORDER ) ) {
+            //var_dump($matches);
+            foreach ( $matches as $shortcode ) {
+                if ( $shortcode[2] === $tag ) {
+                    //$srcs = array();
+                    //var_dump($shortcode[3]);
+                    //echo(htmlspecialchars($shortcode[3]) . "<br>");
+                    /*
+                    if ($parsePosh) {
+                        //need to find and replace the posh quotes with standard ones! 
+                        $shortcode[3]=str_replace("=&#8221;","=\"",$shortcode[3]);
+                        $shortcode[3]=str_replace("&#8221; ","\" ",$shortcode[3]);
+                        //echo(htmlspecialchars(substr($shortcode[3], -7)) . "<br>");
+                        if (substr($shortcode[3], -7)==="&#8221;") {
+                            $shortcode[3]=substr($shortcode[3], 0, -7) . "\"";
+                        };
+                    }
+                    */
+                    //echo(htmlspecialchars($shortcode[3]) . "<br>");
+                    $shortcode_attrs = shortcode_parse_atts( $shortcode[3] );
+                    if ( ! is_array( $shortcode_attrs ) ) {
+                        $shortcode_attrs = array();
+                    }
+                    //var_dump($shortcode_attrs);
+                    //$ret[]=array($shortcode_attrs, $shortcode[5]);
+    
+                    $item = (object) [
+                    'atts' => $shortcode_attrs,
+                    'content' => $shortcode[5]
+                    ];
+    
+                    $ret[]=$item;
+                }
+            }
+        }
+        return $ret;
+    }
+    
 }
 
 function nettop() {
--- settings.php
+++ settings.php
@@ -4,7 +4,7 @@
 
 define('NETTOP_SETTINGS_PAGE', 'nettop-plugin');
 
-class MySettingsPage
+class NettopSettingsPage
 {
     /**
      * Holds the values to be used in the fields callbacks
@@ -20,13 +20,47 @@ class MySettingsPage
         if( is_admin() ) {
             add_action( 'admin_menu', array( $this, 'add_plugin_page' ) );
             add_action( 'admin_init', array( $this, 'page_init' ) );
+
+            add_action('admin_footer', array( $this, 'addTabKeyToTextareas'));    
             //exit;
         }
-
+        
         // Set class property
         $this->options = get_option( NETTOP_SETTINGS_PAGE );
         //var_dump($this->options);
     }
+
+    function addJavascript(){ //in header
+        ?>
+        <script>alert( 'Hi Marmite' ); </script>
+        <?php
+      }
+  
+
+    public function addTabKeyToTextareas() {
+        ?>
+        <script>
+            jQuery(document).delegate('textarea', 'keydown', function(e) {
+            var keyCode = e.keyCode || e.which;
+    
+            if (keyCode == 9) {
+                e.preventDefault();
+                var start = this.selectionStart;
+                var end = this.selectionEnd;
+    
+                // set textarea value to: text before caret + tab + text after caret
+                jQuery(this).val(jQuery(this).val().substring(0, start)
+                            + "\t"
+                            + jQuery(this).val().substring(end));
+    
+                // put caret at right position again
+                this.selectionStart =
+                this.selectionEnd = start + 1;
+            }
+            });
+        </script>
+        <?php
+        }
 
     //externally called - to retrieve one of the config settings
     public function getOption($id) {
@@ -50,6 +84,7 @@ class MySettingsPage
             NETTOP_SETTINGS_PAGE, 
             array( $this, 'create_admin_page' )
         );
+        
     }
 
     /**
@@ -85,16 +120,16 @@ class MySettingsPage
 
         add_settings_section(
             'setting_section_id', // ID
-            'Settings for NettOp plugin', // Title
+            'Settings for NettOp plugin',
             array( $this, 'print_section_info' ), // Callback
             NETTOP_SETTINGS_PAGE // Page
         );  
 
-        //NB todo: maybe refactor to fall in line with the docs: add_settings_field can actually have args added in the 6th argument
+        //NB td: maybe refactor to fall in line with the docs: add_settings_field can actually have args added in the 6th argument
 
         add_settings_field(
             'id_number', // ID
-            'Id', // Title 
+            'Id', 
             function() {echo self::addInputFieldString("id_number", "");},
             NETTOP_SETTINGS_PAGE, // Page
             'setting_section_id' // Section           
@@ -102,16 +137,23 @@ class MySettingsPage
 
         add_settings_field(
             NETTOP_DATA_googleFont, // ID
-            'Google Fonts', // Title 
+            'Google Fonts', 
             function() {echo self::addInputFieldString(NETTOP_DATA_googleFont, "Leave empty to use current theme default.<br>If set adds a 'font-family' css declaration to the header. NB: '" . NETTOP_FALLBACK_FONTS . "' are automatically added as fallbacks.");},
             NETTOP_SETTINGS_PAGE, // Page
             'setting_section_id' // Section           
         );      
 
         add_settings_field(
+            'css', 
+            'CSS page', 
+            function() {echo self::addInputTextField("css", "Type in custom CSS (it will be added in the header). NB! pageCss can override these settings.", 20 , 100);},
+            NETTOP_SETTINGS_PAGE, 
+            'setting_section_id'
+        );      
+
+        add_settings_field(
             'title', 
             'Title', 
-            //array( $this, 'title_callback' ), 
             function() {echo self::addInputFieldString("title", "");},
             NETTOP_SETTINGS_PAGE, 
             'setting_section_id'
@@ -132,8 +174,12 @@ class MySettingsPage
         if( isset( $input[NETTOP_DATA_googleFont] ) )
             $new_input[NETTOP_DATA_googleFont] = sanitize_text_field( $input[NETTOP_DATA_googleFont] );
 
+        if( isset( $input['css'] ) )
+            $new_input['css'] = sanitize_textarea_field( $input['css'] );
+
         if( isset( $input['title'] ) )
             $new_input['title'] = sanitize_text_field( $input['title'] );
+
 
         return $new_input;
     }
@@ -157,7 +203,12 @@ class MySettingsPage
         );
     }
 
-
+    public function addInputTextField($id, $txt, $rows, $columns) {
+        printf(
+            $txt . '<br><textarea rows="' . $rows . '" cols="' . $columns . '" id="' . $id . '" name="' . NETTOP_SETTINGS_PAGE . '[' . $id . ']" >%s</textarea>',
+            isset( $this->options[$id] ) ? esc_attr( $this->options[$id]) : ''
+        );
+    }
 
     public function id_number_callback()
     {
